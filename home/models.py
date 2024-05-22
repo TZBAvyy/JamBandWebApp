c8d3f3d7e8e3e5e60bf5fa9783d9ba8c1a1a62d5
Avi <gibraltar.av@gmail.com>
2024-05-22 10:16:51 +0800

    Created database tables

--- home/models.py
+++ home/models.py
@@ -1,3 +1,93 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
 
-# Create your models here.
+
+class Section(models.Model):
+    instrument = models.CharField(
+            max_length=50, 
+            help_text='Enter a section (e.g. Bass)',
+            validators=[MinLengthValidator(2, "Section instrument must be greater than 1 character")]
+    )
+
+    def __str__(self):
+        """String for representing the Sections object."""
+        return self.instrument
+    
+class Member(models.Model):
+    name = models.CharField(
+            max_length=50, 
+            help_text='Enter a member\'s full name (e.g. Avi)',
+            validators=[MinLengthValidator(2, "Member name must be greater than 1 character")]
+    )
+
+    def __str__(self):
+        """String for representing the Members object."""
+        return self.name
+    
+class MemberSection(models.Model):
+    class Proficiency(models.IntegerChoices):
+        BEGINNER = 1
+        NOVICE = 2
+        ADEPT = 3
+        SKILLED = 4
+        EXPERT = 5
+    
+    proficiency = models.IntegerField(choices=Proficiency)
+
+    section = models.ForeignKey('Section', on_delete=models.CASCADE, null=False)
+
+    member = models.ForeignKey('Member', on_delete=models.CASCADE, null=False)
+
+    def __str__(self):
+        """String for representing the MemberSections object."""
+        return f"{self.member} AS {self.section}"
+
+
+class Event(models.Model):
+    name = models.CharField(
+            max_length=200,
+            validators=[MinLengthValidator(2, "Event name must be greater than 1 character")]
+    )
+    date = models.DateTimeField()
+
+    # Shows up in the admin list
+    def __str__(self):
+        """String for representing the Events object."""
+        return self.name
+    
+class Band(models.Model):
+    name = models.CharField(
+            max_length=200,
+            validators=[MinLengthValidator(2, "Event name must be greater than 1 character")]
+    )
+    event = models.ForeignKey('Event', models.CASCADE, null=False)
+
+    # Shows up in the admin list
+    def __str__(self):
+        """String for representing the Bands object."""
+        return self.name
+    
+class BandMember(models.Model):
+    band = models.ForeignKey('Band', models.CASCADE, null=False)
+
+    section = models.ForeignKey('Section', models.CASCADE, null=False)
+
+    member = models.ForeignKey('Member', models.CASCADE, null=False)
+
+    def __str__(self):
+        """String for representing the BandMembers object."""
+        return f"{self.member} AS {self.section} IN {self.band}"
+    
+class Practice(models.Model):
+    band = models.ForeignKey('Band', models.CASCADE, null=False)
+
+    event = models.ForeignKey('Event', models.CASCADE, null=False)
+
+    startTime = models.DateTimeField()
+
+    endTime = models.DateTimeField()
+
+    def __str__(self):
+        """String for representing the Practices object."""
+        return f"{self.band} USING FOR {self.event} FROM {self.startTime} TO {self.endTime}"
+    
