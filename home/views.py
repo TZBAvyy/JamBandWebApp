fa014bbffecf01544af60a2513826ad4a1d0a4ab
Avi <gibraltar.av@gmail.com>
2024-05-22 19:25:58 +0800

    Prepped Pages for deployment

--- home/views.py
+++ home/views.py
@@ -4,13 +4,17 @@ from django.urls import reverse_lazy
 from django.views.generic import TemplateView
 from django.views import View
 
+from display.models import Event, Practice
+
 # Create your views here.
 
 class Home(LoginRequiredMixin, TemplateView):
     def get(self, request):
         ctx = {
             "description":"",
-            "keywords":""
+            "keywords":"",
+            "event_list":Event.objects.all(),
+            "practice_list":Practice.objects.all(),
         }
         return render(request=request, template_name='home/landing.html', context=ctx)
 
