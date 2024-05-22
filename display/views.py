0d3f033c8310223179503bcbe6b4c62147c84d0e
Avi <gibraltar.av@gmail.com>
2024-05-22 15:22:30 +0800

    Added Section, Members, Event and Practice models listing in /display/

--- display/views.py
+++ display/views.py
@@ -4,15 +4,24 @@ from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from django.views.generic import TemplateView
 from django.views import View
+from django.views.generic.detail import DetailView
 
 from home.models import *
+from django.apps import apps
+
+app = apps.get_app_config('home')
 
 # Create your views here.
 
-class Main(View):
+class Main(LoginRequiredMixin, View):
     ctx = {
         "description":"",
         "keywords":"",
     }
+    
+    for model_name, model in app.models.items():
+        ctx[model_name + "_list"] = model.objects.all()
+
     def get(self, request):
         return render(request=request, template_name="display/main.html", context=self.ctx)
+
