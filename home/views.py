2bb970315c9ad4367c130c41b69db36339a7eb76
Avi <gibraltar.av@gmail.com>
2024-05-22 11:10:47 +0800

    Created base_bootstrap.html, and login.html. Adjusted Home view

--- home/views.py
+++ home/views.py
@@ -1,9 +1,15 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from django.views.generic import TemplateView
+from django.views import View
 
 
 # Create your views here.
 
-class Home(TemplateView):
+class Home(LoginRequiredMixin, TemplateView):
     def get(self, request):
-        return render(request, 'home/landing.html')
+        ctx = {"description":"","keywords":""}
+        return render(request=request, template_name='home/landing.html', context=ctx)
+
