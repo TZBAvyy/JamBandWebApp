929ca96722bf82019b49f6c96e3209f1bcc78539
Avi <gibraltar.av@gmail.com>
2024-06-09 00:44:40 +0800

    Used env variables for better security

--- web_project/settings.py
+++ web_project/settings.py
@@ -24,7 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG", cast=bool)
+
+# SECURITY MEASURES
+SECURE_SSL_REDIRECT = not config("DEBUG", cast=bool)
+SESSION_COOKIE_SECURE = not config("DEBUG", cast=bool)
+CSRF_COOKIE_SECURE = not config("DEBUG", cast=bool)
 
 ALLOWED_HOSTS = ['*']
 
@@ -90,10 +95,10 @@ WSGI_APPLICATION = 'web_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'django-app-db',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': 'db',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
         'PORT': '3306',
     }
 }
