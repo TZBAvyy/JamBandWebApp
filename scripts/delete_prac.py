eddcde5a9733bb98167d1ee41d92fb3f3455663a
Avi <gibraltar.av@gmail.com>
2024-06-20 23:38:57 +0800

    Added script to delete Practice objects that have expired

---
+++ scripts/delete_prac.py
@@ -0,0 +1,11 @@
+from display.models import Practice
+from datetime import datetime as dt
+from time import sleep
+
+def run():
+    print("Running deletion script of expired Practice objects")
+    current_date = dt.now().date()
+    dates_to_delete = Practice.objects.filter(date__lt=current_date)
+    sleep(1)
+    dates_to_delete.delete()
+    print("Deleted!")
