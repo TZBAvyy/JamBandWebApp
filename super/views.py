3333fc3bdf7de38150d69573cac54f1e6bfb606d
Avi <gibraltar.av@gmail.com>
2024-06-24 19:11:18 +0800

    Updated super/views.py

--- super/views.py
+++ super/views.py
@@ -5,14 +5,14 @@ from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from display.models import Event, Band, MemberSection
 # Create your views here.
 
-class SuperView(ListView, LoginRequiredMixin, UserPassesTestMixin):
+class SuperView(LoginRequiredMixin, UserPassesTestMixin, ListView):
     model = Event
     template_name = "super/super.html"
 
     def test_func(self) -> bool | None:
         return self.request.user.is_superuser
 
-class BandDetailView(DetailView, LoginRequiredMixin, UserPassesTestMixin):
+class BandDetailView(LoginRequiredMixin, UserPassesTestMixin, DetailView):
     model = Band
     template_name = "super/band_detail.html"
 
